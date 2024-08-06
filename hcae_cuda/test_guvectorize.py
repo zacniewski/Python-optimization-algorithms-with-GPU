e52d23ada9ccb2ded59bd867802988a22470e7d9
Artur Zacniewski <a.zacniewski@gmail.com>
2024-08-06 21:29:45 +0200

    Test guvectorize

---
+++ hcae_cuda/test_guvectorize.py
@@ -0,0 +1,26 @@
+# Very simple example:
+import numpy as np
+from numba import guvectorize, int64, float64
+
+
+@guvectorize([(int64[:], int64[:], int64[:])], '(n),()->(n)')
+def g(x, y, res):
+    for i in range(x.shape[0]):
+        res[i] = x[i] + y[0]
+
+
+# if using a scalar 'y' it must be declared with 'int64'
+# if using an array it must be declared with 'int64[:]'
+@guvectorize([(int64[:], int64, int64[:])], '(n),()->(n)', target='cuda')
+def g2(x, y, res):
+    for i in range(x.shape[0]):
+        res[i] = x[i] + y
+
+
+a = np.arange(6)
+print(g(a, 10))
+
+b = np.arange(6).reshape(2, 3)
+print(g(b, 10))
+
+print(g2(b, np.array([10, 20])))
