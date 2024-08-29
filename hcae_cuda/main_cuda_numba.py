4008286ba8b96e2cc6bc77208b5103de8ad01e5c
Artur Zacniewski <a.zacniewski@gmail.com>
2024-08-29 23:32:59 +0200

    Preparing kernel from scratch

--- hcae_cuda/main_cuda_numba.py
+++ hcae_cuda/main_cuda_numba.py
@@ -5,10 +5,20 @@ from numba import cuda, float32
 
 from constants import NDM_ROWS, NDM_COLUMNS
 
+threadsperblock = (16, 16)  # Should be a multiple of 32 if possible.
+blockspergrid = (256, 256)  # Blocks per grid
+
+
 @cuda.jit
 def main():
     # start CPU measurement
     init_ndm = cuda.shared.array(shape=(NDM_ROWS, NDM_COLUMNS), dtype=float32)
     x, y = cuda.grid(2)
 
+
 start_cpu = time.perf_counter()
+
+main[blockspergrid, threadsperblock]()
+
+end_cpu = time.perf_counter()
+print(f"\nElapsed time: {end_cpu - start_cpu:.3f} seconds.")
