75bd1d3d9b20c31556a67d1218c6ce4110c97948
Artur Zacniewski <a.zacniewski@gmail.com>
2024-09-12 22:28:04 +0200

    Add GPU reduction

---
+++ numba_cuda_utils/gpu_reduction.py
@@ -0,0 +1,11 @@
+import numpy
+from numba import cuda
+
+@cuda.reduce
+def sum_reduce(a, b):
+    return a + b
+
+A = (numpy.arange(1234, dtype=numpy.float64)) + 1
+expect = A.sum()      # NumPy sum reduction
+got = sum_reduce(A)   # cuda sum reduction
+assert expect == got
