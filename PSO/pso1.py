80669d40f036c7d72139ca1149a0f063e20c88a5
Artur Zacniewski <a.zacniewski@gmail.com>
2024-09-18 21:52:32 +0200

    PSO in progress

--- PSO/pso1.py
+++ PSO/pso1.py
@@ -13,11 +13,29 @@ def function(x):
   return y
 
 
+def populate(size):
+  x1,x2 = -10, 3 #x1, x2 = right and left boundaries of our X axis
+  pop = rnd.uniform(x1,x2, size) # size = amount of particles in population
+  return pop
+
+
+POP_SIZE = 10 #population size
+MAX_ITER = 30 #the amount of optimization iterations
+w = 0.2 #inertia weight
+c1 = 1 #personal acceleration factor
+c2 = 2 #social acceleration factor
+
+
 x = np.arange(-10,3, 0.001)
 y = function(x)
-plt.plot(x,y, lw=3)
-plt.title('Function to optimize')
+
+x1=populate(50)
+y1=function(x1)
+
+plt.plot(x,y, lw=3, label='Func to optimize')
+plt.plot(x1,y1,marker='o', ls='', label='Particles')
 plt.xlabel('x')
 plt.ylabel('y')
+plt.legend()
 plt.grid(True)
 plt.show()
